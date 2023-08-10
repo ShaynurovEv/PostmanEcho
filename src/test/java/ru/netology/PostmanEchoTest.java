c338bfca99d60b75b799d7885c895c6f48267890
Zhenya <shaynurov75@gmail.com>
2023-08-10 23:36:13 +0900

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hi"));
+    }
+}
