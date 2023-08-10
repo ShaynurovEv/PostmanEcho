e2476139ea46d1840ae09a7db31da546afba0aa0
Zhenya <shaynurov75@gmail.com>
2023-08-10 23:43:27 +0900

    True commit

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -9,11 +9,11 @@ public class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Hi")
+                .body("Zhenya")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"));
+                .body("data", equalTo("Zhenya"));
     }
 }
